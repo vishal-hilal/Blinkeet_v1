e05861443a217e3b78b6d9e0c6bbc995d0fa8938
vishal-hilal <vishalhilal1@gmail.com>
2025-08-07 00:07:23 +0530

    cors error

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express()
 
 app.set("view engine","ejs")
 app.use(cors({
-    origin: "https://blinkeet-v1.vercel.app/",
+    origin: "https://blinkeet-v1.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
