ded309f4fa30c5deb3dce3b7ab260e5a16136eb8
vishal-hilal <vishalhilal1@gmail.com>
2025-08-08 18:41:07 +0530

    success url updated

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -237,8 +237,8 @@ export async function stripePaymentController(req, res) {
         },
       ],
       mode: "payment",
-      success_url: `${process.env.BACKEND_URL}+/success`, // backend url
-      cancel_url:`${process.env.BACKEND_URL}+/cancel`, // backend url
+      success_url:"https://blinkeet-v1-t58n.vercel.app/success", // backend url
+      cancel_url:"https://blinkeet-v1-t58n.vercel.app/cancel", // backend url
       metadata: {
         addressId: addressId,
         totalCartItems: cartItems.length,
