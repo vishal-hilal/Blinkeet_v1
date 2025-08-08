b246044c6150e3cdf080fb7d8ac5ef2c49b013c8
vishal-hilal <vishalhilal1@gmail.com>
2025-08-08 14:10:23 +0530

    url changed from onrender to vercel for backend

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://blinkeet-v2.onrender.com" // backend server url
+export const baseURL = "https://blinkeet-v1-t58n.vercel.app" // backend server url
 
 const SummaryApi = {
     register : {
